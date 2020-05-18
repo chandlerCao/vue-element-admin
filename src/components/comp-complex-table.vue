97ae82fc9d01912be690bea3e67a2a1377d01400
学府悦园家用电脑 <597649635@qq.com>
2020-05-18 22:19:14 +0800

    完成表格插槽

--- src/components/comp-complex-table.vue
+++ src/components/comp-complex-table.vue
@@ -1,136 +1,144 @@
 <template>
-	<div class="comp-container">
-		<el-container>
-			<el-header style="height: auto;">
-				<!-- 表格查询表单 -->
-				<comp-form
-					v-if="$attrs['form-data']"
-					ref="queryForm"
-					v-bind="$attrs['form-data']"
-					:submit-disabled.sync="tableDisabled"
-					@submit-form-handler="reloadTableHandle"
-				></comp-form>
-				<!-- 表格自定义按钮 -->
-				<div v-if="Object.keys(tableHeadBtns).length">
-					<el-button
-						v-for="(value, key) in newTableHeadBtns"
-						:key="key"
-						v-bind="value.btn.attrs"
-						@click="value.handler.bind(newTableHeadBtns[key])()"
-					>{{value.btn.name}}</el-button>
-					<slot name="customBtns"></slot>
-				</div>
-			</el-header>
-			<!-- 表格 -->
-			<el-main>
-				<comp-table
-					ref="compTable"
-					v-bind="$attrs['table-data']"
-					:disabled.sync="tableDisabled"
-					:requst-method="$attrs['requst-method']"
-				>
-					<!-- <slot v-for="(slot, slotName) in $slots" :name="slotName" #[slotName] :value="123"></slot> -->
-				</comp-table>
-			</el-main>
-		</el-container>
-		<!-- 表格新增弹窗 -->
-		<comp-modal-form
-			:title="newTableHeadBtns.add.modal.title"
-			:width="newTableHeadBtns.add.modal.width"
-			:visible.sync="newTableHeadBtns.add.modal.visible"
-			:form="newTableHeadBtns.add.form"
-			@submit-form-handler="reloadTableHandle"
-		></comp-modal-form>
-	</div>
+    <div class="comp-container comp-complex-table">
+        <el-container>
+            <el-header style="height: auto;">
+                <!-- 表格查询表单 -->
+                <comp-form
+                    v-if="$attrs['form-data']"
+                    ref="queryForm"
+                    v-bind="$attrs['form-data']"
+                    :submit-disabled.sync="tableDisabled"
+                    @submit-form-handler="reloadTableHandle"
+                ></comp-form>
+                <!-- 表格自定义按钮 -->
+                <div v-if="Object.keys(tableHeadBtns).length">
+                    <el-button
+                        v-for="(value, key) in newTableHeadBtns"
+                        :key="key"
+                        v-bind="value.btn.attrs"
+                        @click="value.handler.bind(newTableHeadBtns[key])()"
+                    >{{value.btn.name}}</el-button>
+                    <slot name="table-custom-btns"></slot>
+                </div>
+            </el-header>
+            <!-- 表格 -->
+            <el-main>
+                <comp-table
+                    ref="compTable"
+                    v-bind="$attrs['table-data']"
+                    :disabled.sync="tableDisabled"
+                    :requst-method="$attrs['requst-method']"
+                >
+                    <template v-for="(slot, slotName) in tableSltos" #[slotName]="{value}">
+                        <slot :name="slotName" :value="value"></slot>
+                    </template>
+                </comp-table>
+            </el-main>
+        </el-container>
+        <!-- 表格新增弹窗 -->
+        <comp-modal-form
+            :title="newTableHeadBtns.add.modal.title"
+            :width="newTableHeadBtns.add.modal.width"
+            :visible.sync="newTableHeadBtns.add.modal.visible"
+            :form="newTableHeadBtns.add.form"
+            @submit-form-handler="reloadTableHandle"
+        ></comp-modal-form>
+    </div>
 </template>
 
 <script>
 import objUtils from '@/utils/obj-utils'
 export default {
-	name: 'compComplexTable',
-	inheritAttrs: false,
-	props: {
-		// 表格头部自定义按钮
-		tableHeadBtns: {
-			type: Object,
-			default() {
-				return {}
-			}
-		}
-	},
-	data() {
-		return {
-			tableDisabled: false,
-			// 枚举表格操作按钮
-			enumTableHeadBtns: {
-				// 新增功能
-				add: {
-					btn: {
-						name: '新增',
-						attrs: {
-							type: 'primary',
-							size: 'mini',
-							icon: 'el-icon-folder-add'
-						}
-					},
-					modal: {
-						title: '新增',
-						visible: false
-					},
-					handler() {
-						// console.log(this)
-						this.modal.visible = true
-					}
-				},
-				// 导出excel
-				export: {
-					btn: {
-						name: '导出',
-						attrs: {
-							type: 'primary',
-							size: 'mini',
-							icon: 'el-icon-download'
-						}
-					},
-					handler: () => {
-						console.log('导出')
-					}
-				}
-			},
-			newTableHeadBtns: {}
-		}
-	},
-	created() {
-		// 合并自定义按钮
-		this.mergeTableHeadBtns()
-	},
-	mounted() {
-		console.log(this.$slots)
-	},
-	methods: {
-		// 合并自定义按钮
-		mergeTableHeadBtns() {
-			if (Object.keys(this.tableHeadBtns)) {
-				const localTableHeadBtns = {}
-				for (const key in this.tableHeadBtns) {
-					if (this.enumTableHeadBtns[key])
-						localTableHeadBtns[key] = this.enumTableHeadBtns[key]
-				}
-				this.newTableHeadBtns = objUtils.assign(
-					localTableHeadBtns,
-					this.tableHeadBtns
-				)
-			}
-		},
-		// 重载表格
-		async reloadTableHandle(formDataVal = {}) {
-			// 重载表格
-			this.$refs.compTable.pagination.currentPage = 1
-			await this.$refs.compTable.reloadTableHandle(
-				Object.create(formDataVal)
-			)
-		}
-	}
+    name: 'compComplexTable',
+    inheritAttrs: false,
+    props: {
+        // 表格头部自定义按钮
+        tableHeadBtns: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+    data() {
+        return {
+            tableDisabled: false,
+            // 枚举表格操作按钮
+            enumTableHeadBtns: {
+                // 新增功能
+                add: {
+                    btn: {
+                        name: '新增',
+                        attrs: {
+                            type: 'primary',
+                            size: 'mini',
+                            icon: 'el-icon-folder-add'
+                        }
+                    },
+                    modal: {
+                        title: '新增',
+                        visible: false
+                    },
+                    handler() {
+                        this.modal.visible = true
+                    }
+                },
+                // 导出excel
+                export: {
+                    btn: {
+                        name: '导出',
+                        attrs: {
+                            type: 'primary',
+                            size: 'mini',
+                            icon: 'el-icon-download'
+                        }
+                    },
+                    handler: () => {
+                        console.log('导出')
+                    }
+                }
+            },
+            newTableHeadBtns: {}
+        }
+    },
+    computed: {
+        tableSltos() {
+            const tableSltos = {}
+            for (const key in this.$scopedSlots) {
+                if (key.includes('table-'))
+                    tableSltos[key] = this.$scopedSlots[key]()
+            }
+            return tableSltos
+        }
+    },
+    created() {
+        // 合并自定义按钮
+        this.mergeTableHeadBtns()
+    },
+    methods: {
+        // 合并自定义按钮
+        mergeTableHeadBtns() {
+            if (Object.keys(this.tableHeadBtns)) {
+                const localTableHeadBtns = {}
+                for (const key in this.tableHeadBtns) {
+                    if (this.enumTableHeadBtns[key])
+                        localTableHeadBtns[key] = this.enumTableHeadBtns[key]
+                }
+                this.newTableHeadBtns = objUtils.assign(
+                    localTableHeadBtns,
+                    this.tableHeadBtns
+                )
+            }
+        },
+        // 重载表格
+        async reloadTableHandle(formDataVal = {}) {
+            // 重载表格
+            this.$refs.compTable.pagination.currentPage = 1
+            await this.$refs.compTable.reloadTableHandle(
+                Object.create(formDataVal)
+            )
+        }
+    }
 }
 </script>
 
--- src/components/comp-table.vue
+++ src/components/comp-table.vue
@@ -1,130 +1,126 @@
 <template>
-	<div class="comp-container comp-table">
-		<el-container>
-			<!-- 表格 -->
-			<el-main>
-				<el-table :data="tableData" height="100%" v-loading="tableDisabled">
-					<el-table-column
-						v-for="item in tableColumn"
-						:key="item.prop"
-						:fixed="item.fixed"
-						:prop="item.prop"
-						:label="item.label"
-						:width="item.width"
-					>
-						<slot
-							v-if="$slots[`table-${item.prop}`]"
-							#defalut="{row}"
-							:name="`table-${item.prop}`"
-							:value="row[item.prop]"
-						>{{row[item.prop]}}</slot>
-					</el-table-column>
-					<!-- 表格操作 -->
-					<el-table-column v-if="tableHandleBtns.length" label="操作">
-						<template slot-scope="scope">
-							<el-button
-								v-for="btnItem in tableHandleBtns"
-								:key="btnItem.name"
-								v-bind="btnItem.attrs"
-								@click="btnItem.handler(scope.$index, scope.row)"
-							>{{btnItem.name}}</el-button>
-						</template>
-					</el-table-column>
-				</el-table>
-			</el-main>
-			<!-- 分页 -->
-			<el-footer style="height: 50px;">
-				<div class="pagination-box">
-					<el-pagination
-						:total="pagination.total"
-						:page-sizes="pagination.pageSizes"
-						:current-page.sync="pagination.currentPage"
-						:page-size.sync="pagination.pageSize"
-						layout="total, sizes, prev, pager, next, jumper"
-						background
-						@size-change="reloadTableHandle({})"
-						@current-change="reloadTableHandle({})"
-						:disabled="tableDisabled"
-					></el-pagination>
-				</div>
-			</el-footer>
-		</el-container>
-	</div>
+    <div class="comp-container comp-table">
+        <el-container>
+            <!-- 表格 -->
+            <el-main>
+                <el-table :data="tableData" height="100%" v-loading="tableDisabled">
+                    <el-table-column
+                        v-for="item in tableColumn"
+                        :key="item.prop"
+                        :fixed="item.fixed"
+                        :prop="item.prop"
+                        :label="item.label"
+                        :width="item.width"
+                    >
+                        <slot
+                            slot-scope="{ row }"
+                            :name="`table-${item.prop}`"
+                            :value="row[item.prop]"
+                        >{{row[item.prop]}}</slot>
+                    </el-table-column>
+                    <!-- 表格操作 -->
+                    <el-table-column v-if="tableHandleBtns.length" label="操作">
+                        <template slot-scope="scope">
+                            <el-button
+                                v-for="btnItem in tableHandleBtns"
+                                :key="btnItem.name"
+                                v-bind="btnItem.attrs"
+                                @click="btnItem.handler(scope.$index, scope.row)"
+                            >{{btnItem.name}}</el-button>
+                        </template>
+                    </el-table-column>
+                </el-table>
+            </el-main>
+            <!-- 分页 -->
+            <el-footer style="height: 50px;">
+                <div class="pagination-box">
+                    <el-pagination
+                        :total="pagination.total"
+                        :page-sizes="pagination.pageSizes"
+                        :current-page.sync="pagination.currentPage"
+                        :page-size.sync="pagination.pageSize"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        background
+                        @size-change="reloadTableHandle({})"
+                        @current-change="reloadTableHandle({})"
+                        :disabled="tableDisabled"
+                    ></el-pagination>
+                </div>
+            </el-footer>
+        </el-container>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'comp-table',
-	props: {
-		tableColumn: {
-			type: Array,
-			default: () => []
-		},
-		tableHandleBtns: {
-			type: Array,
-			default: () => []
-		},
-		// 请求方法
-		requstMethod: {
-			type: Function,
-			default: async () => null
-		},
-		// 遮罩
-		disabled: {
-			type: Boolean,
-			default: false
-		}
-	},
-	data() {
-		return {
-			formDataVal: {}, // 缓存查询表格的表单值
-			// 表格的数据
-			tableData: new Array(20).fill({
-				date: '2020-04-29 17:53:00',
-				name: '曹贱贱',
-				age: 23,
-				sex: '男'
-			}),
-			// 表格的分页信息
-			pagination: {
-				currentPage: 1,
-				total: 50,
-				pageSizes: [10, 20, 30, 40, 50],
-				pageSize: 10
-			},
-			// 表格loading
-			tableDisabled: this.disabled
-		}
-	},
-	watch: {
-		disabled(disabled) {
-			this.tableDisabled = disabled
-		},
-		tableDisabled(tableDisabled) {
-			this.$emit('update:disabled', tableDisabled)
-		}
-	},
-	/* created() {
-		console.log(this.$slots)
-	}, */
-	methods: {
-		// 重载表格
-		async reloadTableHandle(formDataVal) {
-			this.tableDisabled = true
-			await this.requstMethod({
-				pageIndex: this.pagination.currentPage,
-				pageSize: this.pagination.pageSize,
-				...formDataVal
-			})
-			this.tableDisabled = false
-		}
-	}
+    name: 'comp-table',
+    props: {
+        tableColumn: {
+            type: Array,
+            default: () => []
+        },
+        tableHandleBtns: {
+            type: Array,
+            default: () => []
+        },
+        // 请求方法
+        requstMethod: {
+            type: Function,
+            default: async () => null
+        },
+        // 遮罩
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            formDataVal: {}, // 缓存查询表格的表单值
+            // 表格的数据
+            tableData: new Array(20).fill({
+                date: '2020-04-29 17:53:00',
+                name: '曹德健',
+                age: 23,
+                sex: '男'
+            }),
+            // 表格的分页信息
+            pagination: {
+                currentPage: 1,
+                total: 50,
+                pageSizes: [10, 20, 30, 40, 50],
+                pageSize: 10
+            },
+            // 表格loading
+            tableDisabled: this.disabled
+        }
+    },
+    watch: {
+        disabled(disabled) {
+            this.tableDisabled = disabled
+        },
+        tableDisabled(tableDisabled) {
+            this.$emit('update:disabled', tableDisabled)
+        }
+    },
+    methods: {
+        // 重载表格
+        async reloadTableHandle(formDataVal) {
+            this.tableDisabled = true
+            await this.requstMethod({
+                pageIndex: this.pagination.currentPage,
+                pageSize: this.pagination.pageSize,
+                ...formDataVal
+            })
+            this.tableDisabled = false
+        }
+    }
 }
 </script>
 
 <style lang="less">
 .pagination-box {
-	text-align: right;
-	padding-top: 15px;
+    text-align: right;
+    padding-top: 15px;
 }
 </style>
