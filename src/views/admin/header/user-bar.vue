f148fb111c9085a318951b91ba53858104c983e0
学府悦园家用电脑 <597649635@qq.com>
2020-07-02 21:51:56 +0800

    注册

--- src/views/admin/header/user-bar.vue
+++ src/views/admin/header/user-bar.vue
@@ -1,76 +1,80 @@
 <template>
-	<div id="user-bar">
-		<!-- <i class="el-icon-full-screen" id="full-screen-icon" @click="fullScreen"></i> -->
-		<span id="username">{{userInfo.username}}</span>
-		<el-dropdown trigger="click" @command="handleCommand">
-			<!-- 头像展示 -->
-			<span class="el-dropdown-link">
-				<el-avatar size="medium" :src="userInfo.avatar"></el-avatar>
-				<i class="el-icon-arrow-down el-icon--right"></i>
-			</span>
-			<el-dropdown-menu slot="dropdown">
-				<el-dropdown-item command="home">首页</el-dropdown-item>
-				<el-dropdown-item command="github">项目地址</el-dropdown-item>
-				<el-dropdown-item divided command="loginOut">退出登陆</el-dropdown-item>
-			</el-dropdown-menu>
-		</el-dropdown>
-	</div>
+    <div id="user-bar">
+        <!-- <i class="el-icon-full-screen" id="full-screen-icon" @click="fullScreen"></i> -->
+        <span id="username">{{userInfo.username}}</span>
+        <el-dropdown trigger="click" @command="handleCommand">
+            <!-- 头像展示 -->
+            <span class="el-dropdown-link">
+                <el-avatar size="medium" :src="userInfo.avatar"></el-avatar>
+                <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="home">首页</el-dropdown-item>
+                <el-dropdown-item command="github">项目地址</el-dropdown-item>
+                <el-dropdown-item command="register">用户注册</el-dropdown-item>
+                <el-dropdown-item divided command="loginOut">退出登陆</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'user-bar',
-	data() {
-		return {
-			userInfo: {
-				username: '',
-				avatar: ''
-			}
-		}
-	},
-	created() {
-		// 获取用户信息
-		this.getUserInfo()
-	},
-	methods: {
-		// 获取用户信息
-		async getUserInfo() {
-			this.$req(this.$api.user.getUserInfo, {
-				uid: window.localStorage.getItem('uid')
-			}).then(({ username, avatar }) => {
-				this.userInfo.username = username
-				this.userInfo.avatar = avatar
-			})
-		},
-		handleCommand(command) {
-			switch (command) {
-				case 'home':
-					this.$router.push({ name: '首页' })
-					break
-				case 'github':
-					window.open('https://www.baidu.com')
-					break
-				case 'loginOut':
-					this.$router.push('/login')
-			}
-		}
-	}
+    name: 'user-bar',
+    data() {
+        return {
+            userInfo: {
+                username: '',
+                avatar: ''
+            }
+        }
+    },
+    created() {
+        // 获取用户信息
+        this.getUserInfo()
+    },
+    methods: {
+        // 获取用户信息
+        async getUserInfo() {
+            this.$req(this.$api.user.getUserInfo, {
+                uid: window.localStorage.getItem('uid')
+            }).then(({ username, avatar }) => {
+                this.userInfo.username = username
+                this.userInfo.avatar = avatar
+            })
+        },
+        handleCommand(command) {
+            switch (command) {
+                case 'home':
+                    this.$router.push({ name: '首页' })
+                    break
+                case 'github':
+                    window.open('https://www.baidu.com')
+                    break
+                case 'register':
+                    this.$router.push('/register')
+                    break
+                case 'loginOut':
+                    this.$router.push('/login')
+            }
+        }
+    }
 }
 </script>
 
 <style lang="less">
 #user-bar {
-	display: flex;
-	float: right;
-	align-items: center;
-	margin-right: 20px;
+    display: flex;
+    float: right;
+    align-items: center;
+    margin-right: 20px;
 }
 #full-screen-icon {
-	font-size: 25px;
-	padding-right: 15px;
-	cursor: pointer;
+    font-size: 25px;
+    padding-right: 15px;
+    cursor: pointer;
 }
 #username {
-	padding-right: 15px;
+    padding-right: 15px;
 }
 </style>
--- src/views/register.vue
+++ src/views/register.vue
@@ -1,103 +1,120 @@
 <template>
-	<div id="register-form" class="comp-container">
-		<comp-form ref="registerForm" v-bind="registerFormData" @submit-form-handler="registerSubmit"></comp-form>
-	</div>
+    <div id="register-form" class="comp-container">
+        <comp-form
+            ref="registerForm"
+            v-bind="registerFormData"
+            @submit-form-handler="registerSubmit"
+        >
+            <template #form-custom-btns="{ formDataVal }">
+                <el-button
+                    v-if="formDataVal"
+                    type="info"
+                    icon="el-icon-back"
+                    @click="$router.back()"
+                >返回</el-button>
+            </template>
+        </comp-form>
+    </div>
 </template>
 
 <script>
 export default {
-	name: 'register-add',
-	props: {
-		registerData: {
-			type: Object,
-			default() {
-				return {}
-			}
-		}
-	},
-	data() {
-		return {
-			registerFormData: {
-				formData: {
-					username: {
-						label: '用户名称',
-						rule: [
-							{
-								required: true,
-								message: '请输入用户名称',
-								trigger: 'submit'
-							}
-						]
-					},
-					password: {
-						label: '密码',
-						rule: [
-							{
-								required: true,
-								message: '请输入密码',
-								trigger: 'submit'
-							}
-						]
-					},
-					password_confirm: {
-						label: '确认密码',
-						rule: [
-							{
-								required: true,
-								message: '请输入密码',
-								trigger: 'submit'
-							}
-						]
-					},
-					avatar: {
-						label: '用户头像',
-						el: 'comp-upload-image',
-						rule: [
-							{
-								required: true,
-								message: '请上传用户头像',
-								trigger: 'submit'
-							}
-						],
-						attrs: {
-							uploadImgReq: async formData => {
-								return this.$req(
-									this.$api.user.uploadUserAvatar,
-									formData
-								)
-							}
-						}
-					}
-				},
-				submitBtn: {
-					name: '注册'
-				},
-				formAttrs: {
-					labelPosition: 'top'
-				}
-			}
-		}
-	},
-	methods: {
-		// 注册
-		registerSubmit(userFormData) {
-			this.$req(this.$api.user.register, userFormData)
-				.then(({ token, uid }) => {
-					this.$router.push({ name: '首页' })
-					window.localStorage.setItem('token', token)
-					window.localStorage.setItem('uid', uid)
-				})
-				.finally(() => {
-					this.$refs.registerForm.submitLoading = false
-				})
-		}
-	}
+    name: 'register-form',
+    props: {
+        registerData: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+    data() {
+        return {
+            registerFormData: {
+                formData: {
+                    username: {
+                        label: '用户名称',
+                        rule: [
+                            {
+                                required: true,
+                                message: '请输入用户名称',
+                                trigger: 'submit'
+                            }
+                        ]
+                    },
+                    password: {
+                        label: '密码',
+                        rule: [
+                            {
+                                required: true,
+                                message: '请输入密码',
+                                trigger: 'submit'
+                            }
+                        ]
+                    },
+                    password_confirm: {
+                        label: '确认密码',
+                        rule: [
+                            {
+                                required: true,
+                                message: '请输入密码',
+                                trigger: 'submit'
+                            }
+                        ]
+                    },
+                    avatar: {
+                        label: '用户头像',
+                        el: 'comp-upload-image',
+                        rule: [
+                            {
+                                required: true,
+                                message: '请上传用户头像',
+                                trigger: 'submit'
+                            }
+                        ],
+                        attrs: {
+                            uploadImgReq: async formData => {
+                                return this.$req(
+                                    this.$api.user.uploadUserAvatar,
+                                    formData
+                                )
+                            }
+                        }
+                    }
+                },
+                submitBtn: {
+                    name: '注册'
+                },
+                formAttrs: {
+                    labelPosition: 'top'
+                }
+            }
+        }
+    },
+    methods: {
+        // 注册
+        registerSubmit(userFormData) {
+            this.$req(this.$api.user.register, userFormData)
+                .then(({ token, uid }) => {
+                    this.$router.push({ name: '首页' })
+                    window.localStorage.setItem('token', token)
+                    window.localStorage.setItem('uid', uid)
+                })
+                .finally(() => {
+                    try {
+                        this.$refs.registerForm.submitLoading = false
+                    } catch (error) {
+                        error
+                    }
+                })
+        }
+    }
 }
 </script>
 
 <style lang="less">
 #register-form {
-	box-sizing: border-box;
-	padding-left: 15px;
+    box-sizing: border-box;
+    padding-left: 15px;
 }
 </style>
